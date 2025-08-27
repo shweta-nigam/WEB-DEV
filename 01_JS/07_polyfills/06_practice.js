0fb8bbd7801dac6860c540272ba4f17ab17bc05e
Shweta Nigam <shwetanigam2106@gmail.com>
2025-08-27 14:59:34 +0530

    add some and every polyfills

--- 01_JS/07_polyfills/06_practice.js
+++ 01_JS/07_polyfills/06_practice.js
@@ -1,4 +1,4 @@
-// 26th August 2025
+// 26th August 2025   // 27th August 2025
 
 // polyfill of foreach ------------------------------------------------------------------
 
@@ -55,3 +55,77 @@ if(!Array.prototype.myMap){
 }
 const result4 = arr3.myMap(n => n + 2)
 console.log(result4);                   // [3,4,5]
+
+
+// polyfill of some ----------------------------------------------------------------------
+
+// signature: Determines whether the specified callback function returns true for any element of an array. 
+// takes a callback which takes upto 3 arguments, return true 
+// same as find but it returns boolean value instead of current element
+
+const array = [10,20,30]
+const res = array.some((n) => n > 10) 
+console.log(res);    // true 
+
+const res1 = array.some((n) => n > 40) 
+console.log(res1);    // false 
+
+
+// for (let i = 0; i < this.length; i++) {
+//     if (callback(this[i], i, this)) {
+//         return true
+//     } else {
+//         return false   // ❌ this exits on the very first element
+//     }
+// }
+
+
+if(!Array.prototype.mySome){
+    Array.prototype.mySome = function(callback){
+        for (let i = 0; i < this.length; i++) {
+            if(callback(this[i],i,this)){
+                return true
+            }   
+        } 
+        return false
+    }
+}
+
+const res2 = array.mySome((n) => n > 40) 
+console.log(res2); 
+
+const res3 = array.mySome((n) => n > 10) 
+console.log(res3); 
+
+
+
+// polyfill of every ----------------------------------------------------------------------
+
+// signature: false immediately if any element fails the test.
+// true if all elements pass the test.
+
+
+const nums = [1,2,3,4,5]
+const check = nums.every(n => n > 0)
+console.log(check);                        //true
+
+const check2 = nums.every(n => n > 2)
+console.log(check2);                       // false
+
+if(!Array.prototype.myEvery){
+    Array.prototype.myEvery = function(callback){
+
+        for (let i = 0; i < this.length; i++) {
+           if(!callback(this[i],i,this)){
+               return false
+           }
+        }
+        return true
+    }
+}
+
+const check3 = nums.myEvery(n => n > 0)
+console.log(check3);                        //true
+
+const check4 = nums.myEvery(n => n > 2)
+console.log(check4);                       // false
