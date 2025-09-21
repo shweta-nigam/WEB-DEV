eda08936495e14bba9cde991582afb91d4b47bb2
Shweta Nigam <shwetanigam2106@gmail.com>
2025-09-21 22:54:23 +0530

    loop practice

--- 01_JS/04_Loops and iteration/1_P_Q.js
+++ 01_JS/04_Loops and iteration/1_P_Q.js
@@ -7,10 +7,36 @@ function printTriangle(n) {
     let star = "";
     // console.log("when i =", i);
     for (let j = 1; j <= i; j++) {
-      star += "*"    // to attach something at the end of the string we use +=
+      star += "*"; // to attach something at the end of the string we use +=
     }
     console.log(star);
   }
 }
 
 printTriangle(3);
+
+//practice again
+
+
+function printTriangle2(n) {
+  for (let i = 1; i <= n; i++) {
+    let star = "";
+    console.log("round", i);
+    for (let j = 1; j <= i; j++) {
+      star += "*";
+      console.log("j = ", j, "i = ", i); 
+    }
+    console.log(star);
+  }
+}
+
+printTriangle2(3)
+
+
+//notes
+// 🔹 How to remember the difference:
+// ✅ += → “pluses it up” (add to itself).
+// ❌ =+ → looks weird → “equals positive” (just assign as positive number).
+// Another trick:
+// If the + is before the =, it’s an operation.
+// If the + is after the =, it’s just a sign.
